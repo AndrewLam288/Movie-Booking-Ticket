89c3559c0385823a86d87e1b442841c810b1a155
Andrew Lam <lamminhtrunghieu288@gmail.com>
2026-06-22 15:39:43 +0700

    Fix SPA route fallback

--- server/src/main/java/com/andrewlam/server/config/SpaController.java
+++ server/src/main/java/com/andrewlam/server/config/SpaController.java
@@ -1,16 +1,30 @@
 package com.andrewlam.server.config;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaController {
 
-    @GetMapping(
-            value = {"/", "/{*path}"},
-            produces = MediaType.TEXT_HTML_VALUE
-    )
+    @GetMapping({
+            "/movies",
+            "/movies/{movieId}",
+
+            "/cinemas",
+            "/cinemas/{cinemaId}",
+
+            "/showtimes/{showtimeId}",
+
+            "/sign-in",
+            "/register",
+            "/account",
+
+            "/buy-tickets/{movieId}",
+
+            "/checkout/showtimes/{showtimeId}",
+
+            "/booking-confirmed/{bookingCode}"
+    })
     public String forwardToIndex() {
         return "forward:/index.html";
     }
